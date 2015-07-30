82078ee6bcc8a4f61005567bfb53cb72448011d4
jdolera <juanj.dolera@carm.es>
2015-07-30 20:11:32 +0200

    09: Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,26 @@
 var models = require('../models/model.js');
 
-// Get /quizes/question
-exports.question = function(req, res){
-  models.Quiz.findAll().success(function(quiz) {
-    res.render('quizes/question', {pregunta:quiz[0].pregunta});
+// Get /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index.ejs', {quizes: quizes});
   })
 };
 
-// Get /quizes/answer
+// Get /quizes/:id
+exports.show = function(req, res){
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', {quiz:quiz});
+  })
+};
+
+// Get /quizes/:id/answer
 exports.answer = function(req, res){
-  models.Quiz.findAll().success(function(quiz) {
-  if (req.query.respuesta === quiz[0].respuesta){
-    res.render('quizes/answer', {respuesta: 'Correcto:-)'});
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  if (req.query.respuesta === quiz.respuesta){
+    res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto:-)'});
   } else {
-    res.render('quizes/answer', {respuesta: 'Incorrecto!'});
+    res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto!'});
   }
 })
 };
