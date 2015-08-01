e5fe417406df7e1045e4ac2edb02237171b4e88e
jdolera <juanj.dolera@carm.es>
2015-08-01 22:41:46 +0200

    17: Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW para autorizar el acceso
+exports.loginRequired = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
